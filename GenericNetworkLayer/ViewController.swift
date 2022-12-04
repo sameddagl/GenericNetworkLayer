955a57743405bb2bf8bc43419d779cb1f4d66c52
Samed Dağlı <samedddagl@gmail.com>
2022-12-04 21:24:21 +0300

    Initial Commit

---
+++ GenericNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericNetworkLayer
+//
+//  Created by Samed Dağlı on 4.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
